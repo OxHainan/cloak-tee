75863014b532d256e8d799ee6c4ca5aecc4e00ef
thempp66 <914505102@qq.com>
2021-12-10 06:34:24 +0000

    code stylization

--- src/ethereum/account_proxy.h
+++ src/ethereum/account_proxy.h
@@ -2,6 +2,9 @@
 // Licensed under the MIT License.
 #pragma once
 
+// utils
+//#include "app/utils.h"
+
 // EVM-for-CCF
 #include "tables.h"
 
@@ -64,11 +67,9 @@ struct AccountProxy : public eevm::Account, public eevm::Storage {
 
     // SNIPPET_START: store_impl
     void store(const uint256_t& key, const uint256_t& value, const std::string& mpt_id) override {
-        if (!set_reference_kv(mpt_id, key))
-            return false;
-        if (!set_sstore_kv(mpt_id, key, value))
-            return false;
-        return storage.put(translate(key), value);
+        set_reference_kv(mpt_id, key);
+        set_sstore_kv(mpt_id, key, value);
+        storage.put(translate(key), value);
     }
     // SNIPPET_END: store_impl
 
@@ -81,19 +82,19 @@ struct AccountProxy : public eevm::Account, public eevm::Storage {
         return storage.remove(translate(key));
     }
 
-    bool get_hash_state(const uint256_t& k, HashState& hash_state) {
+    bool get_hash_state(const uint256_t& key, HashState& hash_state) {
         bool exist = false;
         uint256_t exist_key;
-        for (auto it = hash_states.begin(); it != hash_states.end(); it++) {
-            if (k.hi == it->first.hi) {
+        for (auto& [k, v] : hash_states) {
+            if (key.hi == k.hi) {
                 exist = true;
-                exist_key = it->first;
+                exist_key = k;
             }
         }
         if (exist) {
-            hash_states[k] = hash_states[exist_key];
-            hash_states[k].addr = k;
-            hash_state = hash_states[k];
+            hash_states[key] = hash_states[exist_key];
+            hash_states[key].addr = key;
+            hash_state = hash_states[key];
             return true;
         }
         return false;
@@ -119,7 +120,7 @@ struct AccountProxy : public eevm::Account, public eevm::Storage {
         var_info.addr = key;
     }
 
-    bool set_reference_kv(const std::string& mpt_id, const uint256_t& key) {
+    void set_reference_kv(const std::string& mpt_id, const uint256_t& key) {
         HashState hash_state;
         bool exist = get_hash_state(key, hash_state);
         VarInfo var_info;
@@ -130,34 +131,39 @@ struct AccountProxy : public eevm::Account, public eevm::Storage {
             // non-dynamic type
             static_type_to_var_info(key, var_info);
         }
-        std::cout << "[set_reference_kv]"
-                  << "mpt_id=" << mpt_id << ",var_type=" << var_info.var_type
-                  << ",addr=" << var_info.addr << ",key=" << var_info.key
-                  << ",slot=" << var_info.slot << std::endl;
-        std::string v;
-        serialize_var_info(var_info, v);
-        reference_kv.put(std::make_pair(mpt_id, key), v);
-        return reference_kv.put(std::make_pair(mpt_id, key), v);
+        CLOAK_DEBUG_FMT("set_reference_kv, mpt_id:{}, var_type:{}, addr:{}, key:{}, slot:{}",
+                        mpt_id,
+                        var_info.var_type,
+                        var_info.addr,
+                        var_info.key,
+                        var_info.slot);
+        nlohmann::json j = var_info;
+        reference_kv.put(std::make_pair(mpt_id, key), j.dump());
     }
 
     bool get_reference_kv(const std::string& mpt_id, const uint256_t& key, VarInfo& var_info) {
         std::string var_info_json = reference_kv.get(std::make_pair(mpt_id, key)).value_or("");
         if (var_info_json == "") {
-            std::cout << "[get_reference_kv]"
-                      << "mpt_id=" << mpt_id << ",var_type=" << var_info.var_type
-                      << ",addr=" << var_info.addr << ",key=" << var_info.key
-                      << ",slot=" << var_info.slot << std::endl;
+            CLOAK_DEBUG_FMT("get_reference_kv, mpt_id:{}, var_type:{}, addr:{}, key:{}, slot:{}",
+                            mpt_id,
+                            var_info.var_type,
+                            var_info.addr,
+                            var_info.key,
+                            var_info.slot);
             return false;
         }
-        deserialize_var_info(var_info_json, var_info);
-        std::cout << "[get_reference_kv]"
-                  << "mpt_id=" << mpt_id << ",var_type=" << var_info.var_type
-                  << ",addr=" << var_info.addr << ",key=" << var_info.key
-                  << ",slot=" << var_info.slot << std::endl;
+        nlohmann::json j = nlohmann::json::parse(var_info_json);
+        var_info = j.get<VarInfo>();
+        CLOAK_DEBUG_FMT("get_reference_kv, mpt_id:{}, var_type:{}, addr:{}, key:{}, slot:{}",
+                        mpt_id,
+                        var_info.var_type,
+                        var_info.addr,
+                        var_info.key,
+                        var_info.slot);
         return true;
     }
 
-    bool set_sstore_kv(const std::string& mpt_id, const uint256_t& key, const uint256_t& value) {
+    void set_sstore_kv(const std::string& mpt_id, const uint256_t& key, const uint256_t& value) {
         HashState hash_state;
         bool exist = get_hash_state(key, hash_state);
         VarInfo var_info;
@@ -169,48 +175,40 @@ struct AccountProxy : public eevm::Account, public eevm::Storage {
             // non-dynamic type
             static_type_to_var_info(key, var_info);
         }
-        std::cout << "[set_sstore_kv]"
-                  << "mpt_id=" << mpt_id << ",var_type=" << var_info.var_type
-                  << ",addr=" << var_info.addr << ",key=" << var_info.key
-                  << ",value=" << var_info.value << ",slot=" << var_info.slot << std::endl;
-        std::string v;
-        serialize_var_info(var_info, v);
-        return sstore_kv.put(std::make_pair(mpt_id, key), v);
+        CLOAK_DEBUG_FMT("set_sstore_kv, mpt_id:{}, var_type:{}, addr:{}, key:{}, value:{}, slot:{}",
+                        mpt_id,
+                        var_info.var_type,
+                        var_info.addr,
+                        var_info.key,
+                        var_info.value,
+                        var_info.slot);
+        nlohmann::json j = var_info;
+        sstore_kv.put(std::make_pair(mpt_id, key), j.dump());
     }
 
     bool get_sstore_kv(const std::string& mpt_id, const uint256_t& key, VarInfo& var_info) {
         std::string var_info_json = sstore_kv.get(std::make_pair(mpt_id, key)).value_or("");
         if (var_info_json == "") {
-            std::cout << "[get_sstore_kv]"
-                      << "mpt_id=" << mpt_id << ",var_type=" << var_info.var_type
-                      << ",addr=" << var_info.addr << ",key=" << var_info.key
-                      << ",slot=" << var_info.slot << std::endl;
+            CLOAK_DEBUG_FMT(
+                "get_sstore_kv, mpt_id:{}, var_type:{}, addr:{}, key:{}, value:{}, slot:{}",
+                mpt_id,
+                var_info.var_type,
+                var_info.addr,
+                var_info.key,
+                var_info.value,
+                var_info.slot);
             return false;
         }
-        deserialize_var_info(var_info_json, var_info);
-        std::cout << "[get_sstore_kv]"
-                  << "mpt_id=" << mpt_id << ",var_type=" << var_info.var_type
-                  << ",addr=" << var_info.addr << ",key=" << var_info.key
-                  << ",slot=" << var_info.slot << std::endl;
+        nlohmann::json j = nlohmann::json::parse(var_info_json);
+        var_info = j.get<VarInfo>();
+        CLOAK_DEBUG_FMT("get_sstore_kv, mpt_id:{}, var_type:{}, addr:{}, key:{}, value:{}, slot:{}",
+                        mpt_id,
+                        var_info.var_type,
+                        var_info.addr,
+                        var_info.key,
+                        var_info.value,
+                        var_info.slot);
         return true;
-    }
-
-    void serialize_var_info(const VarInfo& var_info, std::string& v) {
-        nlohmann::json j{{"var_type", var_info.var_type},
-                         {"addr", var_info.addr},
-                         {"key", var_info.key},
-                         {"value", var_info.value},
-                         {"slot", var_info.slot}};
-        v = j.dump();
-    }
-
-    void deserialize_var_info(const std::string& str, VarInfo& var_info) {
-        nlohmann::json j = nlohmann::json::parse(str);
-        var_info.var_type = j["var_type"];
-        var_info.addr = j["addr"];
-        var_info.key = j["key"];
-        var_info.value = j["value"];
-        var_info.slot = j["slot"];
     }
 };
 } // namespace Ethereum
